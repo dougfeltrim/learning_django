a8e82c6ca8004a317fb62097818e01a42c2f463e
dougfeltrim <dougfeltrim@gmail.com>
2018-01-22 18:00:07 -0200

    detail add path

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
 
 
 '''def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 '''
-def detail(request, question_id):
+'''def detail(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
-
+'''
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
@@ -19,8 +22,9 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
