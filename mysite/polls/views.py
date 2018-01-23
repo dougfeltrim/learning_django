3585a849977d87ff2789a3c4a5887d39b3f1fcee
dougfeltrim <dougfeltrim@gmail.com>
2018-01-23 14:47:41 -0200

    Finished part 3 writing views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,13 +6,6 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 
-
-'''def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-'''
-'''def detail(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
-'''
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
