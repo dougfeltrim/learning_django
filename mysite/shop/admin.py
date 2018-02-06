9cfe311efa55018dc5e52adfaf4167c50463f06a
dougfeltrim <dougfeltrim@gmail.com>
2018-02-06 15:47:37 -0200

    Created models to shop

--- mysite/shop/admin.py
+++ mysite/shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Sell
+
+admin.site.register(Sell)
 # Register your models here.
--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Sell(models.Model):
+    sell_name = models.CharField(max_length=200)
+    #pub_date = models.DateTimeField('date published')
+    #sell_image = models.ImageField()
+    sell_price = models.DecimalField(max_digits=6, decimal_places=2)
+    sell_amount = models.IntegerField(default=0)
+    sell_description = models.TextField(default="description")
+
+    def __str__(self):
+        return self.sell_name
