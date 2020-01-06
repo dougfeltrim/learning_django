067d56854d7bfd5e3ba6e07e59990da985ec634e
Convertize <convertize@iMac-de-Convertize.local>
2020-01-06 15:09:16 -0800

    Clean for new tutorial

--- machine/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/douglasfeltrim/projects/learning_django/machine/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- mysite/shop/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Sell
-
-admin.site.register(Sell)
-# Register your models here.
