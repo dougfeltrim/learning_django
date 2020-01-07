1d08604c95a3191cc7bcee7b1d18cacc06c3720b
dougfeltrim <dougfeltrim@gmail.com>
2020-01-07 16:53:52 -0300

    Finalizado o sistema de votacao e contagem dos resultados da votacao

--- tutorial_django/mysite/polls/views.py
+++ tutorial_django/mysite/polls/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import Choice, Question
 
 def index(request):
-    return HttpResponse("Ola mundo. Voce esta no index de polls")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk= question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #redisplay th question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didin't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
